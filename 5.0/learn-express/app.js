e46846b7c8abfb65f8a7136e7e73709783c1c3f0
MeowMeowPuppy <woojw6012@gmail.com>
2023-09-08 21:17:40 +0900

    exrpress에서 html 띄우기

--- 5.0/learn-express/app.js
+++ 5.0/learn-express/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
-app.set('port',process.env.PORT||3000);
+app.set('port',process.env.PORT||3000); // 환경변수가 없으면 3000번을 포트 번호로 사용
 
 app.get('/',(req,res)=>{
-    res.send('Hello Express');
+    // res.send('Hello Express') send()로 문자열을 보냈다
+    res.sendFile(path.join(__dirname,'./index.html')); // sendFile() 으로 파일을 전달했다
 })
 
 app.listen(app.get('port'),()=>{
