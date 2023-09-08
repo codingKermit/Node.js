66887a1d5a15f85919e4aa03aa4d260ac6e25435
MeowMeowPuppy <woojw6012@gmail.com>
2023-09-08 20:34:11 +0900

    cluster 종료 확인해보기

--- cluster.js
+++ cluster.js
@@ -19,6 +19,9 @@ if(cluster.isMaster){
         res.writeHead(200,{'Content-Type':'text/html; charset=utf-8'});
         res.write('<h1>Hello Node!</h1>');
         res.end('<p>Hello Cluster!</p>');
+        setTimeout(() => { // 워커가 존재하는지 확인하기 위해 1초마다 강제 종료
+            process.exit(1);
+        }, 1000);
     }).listen(8086);
 
     console.log(`${process.pid}번 워커 실행`)
