7062155ee8610ddbc649a80d6d6423eb07fe5a31
MeowMeowPuppy <woojw6012@gmail.com>
2023-09-12 19:30:11 +0900

    미들웨어 한번에 장착하기

--- 5.0/learn-express/app.js
+++ 5.0/learn-express/app.js
@@ -9,21 +9,41 @@ dotenv.config();
 const app = express();
 app.set('port',process.env.PORT||3000); // 환경변수가 없으면 3000번을 포트 번호로 사용
 
-app.use(morgan('tiny'));
-app.use('/',express.static(path.join(__dirname,'public')));
-app.use(express.json());
-app.use(express.urlencoded({extended : false}));
-app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
-    resave : false,
-    saveUninitialized : false,
-    secret : process.env.COOKIE_SECRET,
-    cookie : {
-        httpOnly : true,
-        secure : true,
-    },
-    name : 'session-cookie'
-}));
+
+// 아래 처럼 한번에 여러개의 미들웨어를 장착시킬 수 있다
+app.use(
+    morgan('dev'),
+    express.static('/',path.join(__dirname,'public')),
+    express.json(),
+    express.urlencoded({extended:false}),
+    cookieParser(process.env.COOKIE_SECRET),
+    session({
+        resave : false,
+        saveUninitialized : false,
+        secret : process.env.COOKIE_SECRET,
+        cookie:{
+            httpOnly : true,
+            secure : false
+        },
+        name : 'session-cookie'
+    }),
+)
+
+// app.use(morgan('dev'));
+// app.use('/',express.static(path.join(__dirname,'public')));
+// app.use(express.json());
+// app.use(express.urlencoded({extended : false}));
+// app.use(cookieParser(process.env.COOKIE_SECRET));
+// app.use(session({
+//     resave : false,
+//     saveUninitialized : false,
+//     secret : process.env.COOKIE_SECRET,
+//     cookie : {
+//         httpOnly : true,
+//         secure : true,
+//     },
+//     name : 'session-cookie'
+// }));
 
 
 app.use((req,res,next)=>{
