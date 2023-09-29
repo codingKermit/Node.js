96190957001fceae7c29b42a710803a80af07aea
MeowMeowPuppy <woojw6012@gmail.com>
2023-09-30 00:03:38 +0900

    passport 모듈 사용해서 로그인 구조 만들기

--- 9.0/nodebird/passport/index.js
+++ 9.0/nodebird/passport/index.js
@@ -4,13 +4,24 @@ const kakao = require('passport-kakao');
 const User = require('../models/user');
 
 module.exports = () =>{
+    /* 
+    "로그인 시" 실행. 세션에 어떤 데이터를 저장할지 정하는 메서드
+    매개 변수로 user를 받는다
+    */
     passport.serializeUser((user,done)=>{
+        // done 함수의 첫번째에는 에러 발생시 사용, 
+        // 두번째는 저장하고 싶은 데이터
         done(null,user.id);
     });
 
+
+    /* 
+    "각 요청마다" 실행
+    serializeUser의 done에서 사용된 두번째 데이터가 여기서 인자로 사용
+    */
     passport.deserializeUser((id,done)=>{
         user.findOne({where:{id}})
-        .then(user=>done(null,user))
+        .then(user=>done(null,user)) // 에러 발생시 => null, 정상 동작시 req.user에 정보 저장
         .catch(err => done(err));
     });
 
--- 9.0/nodebird/routes/page.js
+++ 9.0/nodebird/routes/page.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const { isLoggedIn, isNotLoggedIn } = require('../middlewares');
 const { renderProfile, renderJoin, renderMain } = require('../controllers/page');
 
 const router = express.Router();
 
 router.use((req,res,next)=>{
+    req.locals.user = req.user;
     res.locals.user = null;
     res.locals.followerCount = 0;
     res.locals.follwingCount = 0;
@@ -11,8 +13,10 @@ router.use((req,res,next)=>{
     next();
 })
 
-router.get('/profile',renderProfile);
-router.get('/join',renderJoin);
+// isLoggedIn의 내부에서 isAuthenticated() 값이 true일 경우 next가 동작하여 renderProfile로 넘어간다.
+// 하지만 false일 경우 next()는 동작하지 않고 '로그인 필요' 라는 문구가 나온다
+router.get('/profile', isLoggedIn, renderProfile); 
+router.get('/join', isNotLoggedIn, renderJoin);
 router.get('/',renderMain);
 
 module.exports = router;
