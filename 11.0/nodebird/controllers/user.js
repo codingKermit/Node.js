232729eb4fb08479581ff6b6745e8a839a7820ef
codingKermit <woojw6012@gmail.com>
2023-12-03 19:11:14 +0900

    11.2 노트 테스트 코드 작성
    이후 진행을 위한 코드 롤백

--- 11.0/nodebird/controllers/user.js
+++ 11.0/nodebird/controllers/user.js
@@ -1,12 +1,12 @@
-const { follow } = require('../service/user');
 const User = require('../models/user');
 
 exports.follow = async (req,res,next) => {
     try {
-        const result = await follow(req.user.id,req.params.id);
-        if(result === 'ok'){
-            res.send('success');
-        } else if ( result === 'no user'){
+        const user = await User.findOne({where : { id : req.user.id }});
+        if(user){
+            await user.addFollowing(parseInt(req.params.id,10));
+            res.send('success')
+        } else {
             res.status(404).send('no user');
         }
     } catch (error) {
